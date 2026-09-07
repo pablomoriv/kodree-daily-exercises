e925f554a56267497653f06c4f0ce6abbfad33f0
Pablo Moreno <326254454+pablomoriv@users.noreply.github.com>
2026-09-07 17:19:40 -0600

    feat: Completando ejercicio 02 - select columns

---
+++ 01.SQL/01.SELECT/02.select_columns.sql
@@ -0,0 +1,5 @@
+-- De la tabla books mostrar tanto el título como el año de publicación de todos los libros. Escribe una sentencia SELECT para mostrar la información de las columnas 'title' y 'publication_year'
+SELECT
+    title,
+    publication_year
+FROM books;
